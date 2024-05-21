57d0cbf08c12134d03eb9e35c3aeb2c2e7541259
d-gunde <deekshagunde2022@gmail.com>
2024-05-21 13:22:46 -0400

    reverting dbt project changes

--- airflow-mwaa/dags/dbt/dbt-starter-project/dbt/tests/consistent_created_at.sql
+++
@@ -1,5 +0,0 @@
-SELECT * FROM
-    {{ref('dim_listings_cleansed')}} AS listings
-JOIN {{ ref('fct_reviews')}} AS fct
-ON listings.listing_id = fct.listing_id
-WHERE listings.created_at >= fct.review_date
--- airflow-mwaa/dags/dbt/dbt-starter-project/dbt/tests/dim_listings_minimum_nights.sql
+++
@@ -1,8 +0,0 @@
-SELECT * FROM 
-    {{ref("dim_listings_cleansed")}}
-WHERE 
-    minimum_nights < 1
-LIMIT 10
-
-
-{# {{ test_positive_value( ref("dim_listings_cleansed"), 'minimum_nights')}} #}
